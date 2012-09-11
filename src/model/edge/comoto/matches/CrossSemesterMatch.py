fa76345bbdad6c5dc7644d7095e2f8cf684a7f89
Jon Tedesco <jon@jontedesco.net>
2012-09-10 23:56:22 -0500

    Added cross semester match

---
+++ src/model/edge/comoto/matches/CrossSemesterMatch.py
@@ -0,0 +1,8 @@
+from src.model.edge.comoto.Match import Match
+
+__author__ = 'jontedesco'
+
+class SameSemesterMatch(Match):
+    """
+      Represents a match from different semesters between two students
+    """
