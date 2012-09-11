f0e6b4c844bd270d3ba81cc52893b74e9a8cf2e6
Jon Tedesco <jon@jontedesco.net>
2012-09-11 11:19:39 -0500

    Added unit tests for ArnetMinerDataImporter text parsing

--- test/importers/ArnetMinerDataImporterTest.py
+++ test/importers/ArnetMinerDataImporterTest.py
@@ -1,5 +1,6 @@
 import unittest
-from src.importers.ArnetMinerDataImporter import ArnetMinerDataImporter
+from src.importer.ArnetMinerDataImporter import ArnetMinerDataImporter
+from src.importer.error import ArnetParseError
 
 __author__ = 'jontedesco'
 
@@ -10,22 +11,173 @@ class ArnetMinerDataImporterTest(unittest.TestCase):
 
     def setUp(self):
 
+        """
+
+        """
         self.dataImporter = ArnetMinerDataImporter(None, None)
 
-        pass
+    def testParsePapersWithoutCitationsInput(self):
 
-    def testParseSimplePapersWithoutCitationsInput(self):
-        # Test both -1 and 0 for citations
-        pass
+        papersWithoutCitationsInput = """
+            #*Some paper title
+            #@Author One
+            #year1995
+            #confModern Database Systems
+            #citation-1
+            #index0
+            #arnetid1
 
-    def testParseSimplePapersWithCorrectCitationsInput(self):
-        pass
+            #*Some other paper title
+            #@Author Two
+            #year1999
+            #confModern Database Systems
+            #citation0
+            #index1
+            #arnetid2
+            #!Some really long abstract
+        """
 
-    def testParseSimplePapersWithInvalidCitationsInput(self):
-        pass
+        expectedParsedData = {
+            0: {
+                'id': 0,
+                'arnetid': 1,
+                'author': 'Author One',
+                'conference': 'Modern Database Systems',
+                'references': [],
+                'title': 'Some paper title',
+                'year': 1995
+            },
+            1: {
+                'id': 1,
+                'arnetid': 2,
+                'author': 'Author Two',
+                'conference': 'Modern Database Systems',
+                'references': [],
+                'title': 'Some other paper title',
+                'year': 1999
+            }
+        }
 
-    def testMissingPaperAttributesInput(self):
-        pass
+        actualParsedData = self.dataImporter.parseInputContent(papersWithoutCitationsInput)
 
-    def testExtraPaperAttributesInput(self):
-        pass
+        self.assertDictEqual(actualParsedData, expectedParsedData)
+
+
+    def testParsePapersWithCorrectCitationsInput(self):
+
+        papersWithCorrectCitationsInput = """
+            #*Some paper title
+            #@Author One
+            #year1999
+            #confModern Database Systems
+            #citation1
+            #index0
+            #arnetid1
+            #%1
+            #!Some really long abstract
+
+            #*Some other paper title
+            #@Author Two
+            #year1995
+            #confModern Database Systems
+            #citation2
+            #index1
+            #arnetid2
+
+            #*Yet another paper title
+            #@Author Three
+            #year2005
+            #confData Mining
+            #citation0
+            #index2
+            #arnetid3
+            #%0
+            #%1
+        """
+
+        expectedParsedData = {
+            0: {
+                'id': 0,
+                'arnetid': 1,
+                'author': 'Author One',
+                'conference': 'Modern Database Systems',
+                'references': [1],
+                'title': 'Some paper title',
+                'year': 1999
+            },
+            1: {
+                'id': 1,
+                'arnetid': 2,
+                'author': 'Author Two',
+                'conference': 'Modern Database Systems',
+                'references': [],
+                'title': 'Some other paper title',
+                'year': 1995
+            },
+            2: {
+                'id': 2,
+                'arnetid': 3,
+                'author': 'Author Three',
+                'conference': 'Data Mining',
+                'references': [0,1],
+                'title': 'Yet another paper title',
+                'year': 2005
+            }
+        }
+
+        actualParsedData = self.dataImporter.parseInputContent(papersWithCorrectCitationsInput)
+
+        self.assertDictEqual(actualParsedData, expectedParsedData)
+
+
+    def testParsePapersWithInvalidCitationsInput(self):
+
+        papersWithInvalidCitationsInput = """
+            #*Some paper title
+            #@Author One
+            #year1995
+            #confModern Database Systems
+            #citation-1
+            #index0
+            #arnetid1
+            #%4
+            #%5
+
+            #*Some other paper title
+            #@Author Two
+            #year1999
+            #confModern Database Systems
+            #citation0
+            #index1
+            #arnetid2
+        """
+
+        try:
+            self.dataImporter.parseInputContent(papersWithInvalidCitationsInput)
+            self.fail("Should have failed to parse input with invalid citations")
+        except ArnetParseError:
+            pass
+
+
+    def testParseMissingPaperAttributesInput(self):
+
+        papersWithMissingAttributesInput = """
+            #*Some paper title
+            #@Author One
+            #year1995
+            #arnetid1
+
+            #*Some other paper title
+            #@Author Two
+            #year1999
+            #confModern Database Systems
+            #citation0
+            #index1
+            #arnetid2
+        """
+
+        try:
+            self.dataImporter.parseInputContent(papersWithMissingAttributesInput)
+            self.fail("Should have failed to parse input with missing attributes")
+        except ArnetParseError:
+            pass
