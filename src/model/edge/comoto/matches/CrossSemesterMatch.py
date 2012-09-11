3e97111f00f478d46a91eebfab7f648410323fa7
Jon Tedesco <jon@jontedesco.net>
2012-09-11 01:00:20 -0500

    Fixed class name in CrossSemesterMatch

--- src/model/edge/comoto/matches/CrossSemesterMatch.py
+++ src/model/edge/comoto/matches/CrossSemesterMatch.py
@@ -2,7 +2,7 @@ from src.model.edge.comoto.Match import Match
 
 __author__ = 'jontedesco'
 
-class SameSemesterMatch(Match):
+class CrossSemesterMatch(Match):
     """
       Represents a match from different semesters between two students
     """
