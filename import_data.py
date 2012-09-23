0ede0ddbd4b0165f42bebce9823abf8a385fac4b
Jon Tedesco <jon@jontedesco.net>
2012-09-23 15:13:37 -0500

    Added PROJECT_ROOT env var in import_data script

--- import_data.py
+++ import_data.py
@@ -1,16 +1,13 @@
+import os
 from src.importer.ArnetMinerDataImporter import ArnetMinerDataImporter
-from src.importer.CoMoToDataImporter import CoMoToDataImporter
 
 __author__ = 'jon'
 
 # Imports all experimental data from files in 'data/' directory, and outputs as pickled NetworkX objects in 'graphs/'
 if __name__ == '__main__':
 
-    # Get UIUC login info from user (for CoMoTo)
-    netid = raw_input("Netid:")
-    password = raw_input("Password:")
+    os.putenv('PROJECT_ROOT', os.getcwd())
 
-    comotoDataImporter = CoMoToDataImporter('graphs/dblp-arnet-v5', netid, password)
-    arnetMinerDataImporter = ArnetMinerDataImporter('data/DBLP-citation-Feb21.txt', 'graphs/arnetV5WithPublicationLinks')
-    comotoDataImporter.start()
+    #comotoDataImporter = CoMoToDataImporter('graphs/dblp-arnet-v5', netid, password)
+    arnetMinerDataImporter = ArnetMinerDataImporter('data/real/DBLP-citation-Feb21.txt', 'graphs/arnetV5WithPublicationLinks')
     arnetMinerDataImporter.start()
