f0b0b403b69963f48cef8e3cdd02dbb06bd42f84
Jon Tedesco <jon@jontedesco.net>
2012-09-23 15:18:13 -0500

    Removing programmatic implementation of environment variables

--- import_data.py
+++ import_data.py
@@ -1,12 +1,9 @@
-import os
 from src.importer.ArnetMinerDataImporter import ArnetMinerDataImporter
 
 __author__ = 'jon'
 
 # Imports all experimental data from files in 'data/' directory, and outputs as pickled NetworkX objects in 'graphs/'
 if __name__ == '__main__':
-
-    os.putenv('PROJECT_ROOT', os.getcwd())
 
     #comotoDataImporter = CoMoToDataImporter('graphs/dblp-arnet-v5', netid, password)
     arnetMinerDataImporter = ArnetMinerDataImporter('data/real/DBLP-citation-Feb21.txt', 'graphs/arnetV5WithPublicationLinks')
