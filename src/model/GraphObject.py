8a801716a9ad955efafbc8332c441fad8877f722
Jon Tedesco <jon@jontedesco.net>
2012-10-15 18:58:41 -0700

    Sped up GraphObject dictionary conversion

--- src/model/GraphObject.py
+++ src/model/GraphObject.py
@@ -11,20 +11,22 @@ class GraphObject(object):
 
             @param  id  Guaranteed to be unique amongst objects of the same type
         """
-
         if id is not None:
             self.id = id
+        self.dictionary = None
 
 
     def toDict(self):
         """
           Returns a dictionary containing all data for this object
         """
+        try:
+            return self.dictionary
+        except AttributeError:
+            self.dictionary = self.__dict__
+            self.dictionary['type'] = self.__class__.__name__
+            return self.dictionary
 
-        dictionary = self.__dict__
-        dictionary['type'] = self.__class__.__name__
-
-        return dictionary
 
     def __eq__(self, other):
         return self.toDict() == other.toDict()
