5c25c8d8732860c6ff9f968eb474b5af8e1b0634
Jon Tedesco <jon@jontedesco.net>
2013-01-31 21:53:57 -0600

    Updated comment in pagerank distance strategy

--- src/similarity/heterogeneous/PageRankDistanceStrategy.py
+++ src/similarity/heterogeneous/PageRankDistanceStrategy.py
@@ -4,7 +4,7 @@ __author__ = 'jontedesco'
 
 class PageRankDistanceStrategy(GlobalInfluenceDistanceStrategy):
     """
-      Class that computes the absolute difference between some influence measure of nodes
+      Class that computes the absolute difference of PageRank scores between some influence measure of nodes
     """
 
     def getGlobalInfluenceMeasure(self, projectedGraph):
