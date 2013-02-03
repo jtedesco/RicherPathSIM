a8830ecdc0ca30fca17cd873ff40c539ce4f3b86
Jon Tedesco <jon@jontedesco.net>
2013-02-03 13:37:58 -0600

    Removed SimRank & RW homogeneous strategies

--- src/similarity/homogeneous/RandomWalkStrategy.py
+++
@@ -1,8 +0,0 @@
-from src.similarity.SimilarityStrategy import SimilarityStrategy
-
-__author__ = 'jontedesco'
-
-class RandomWalkStrategy(SimilarityStrategy):
-    """
-      Implementation of simple random walk algorithm for node similarity in a homogeneous graph
-    """
