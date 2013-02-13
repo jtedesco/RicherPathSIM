5a8842a6924b9d9fe608568e979ba5970775693d
Jon Tedesco <jon@jontedesco.net>
2013-02-12 23:24:47 -0600

    Parameterized APCPA experiment to run faster

--- experiment/real/four_area/barebones/EfficientAPCPAPathsimExperiment.py
+++ experiment/real/four_area/barebones/EfficientAPCPAPathsimExperiment.py
@@ -10,22 +10,10 @@ class EfficientAPCPAPathSimExperiment(Experiment):
       Runs some experiments with PathSim on author similarity for the 'four area' dataset
     """
 
-    def runFor(self, author):
-
-        # Parse 4-area dataset graph
-        graph, nodeIndex = parseFourAreaDataset()
-
-        # Compute APCPA adjacency matrix
-        apcAdjMatrix, extraData = getMetaPathAdjacencyData(graph, nodeIndex, ['author', 'paper', 'conference'])
-        cpaAdjMatrix, data = getMetaPathAdjacencyData(graph, nodeIndex, ['conference', 'paper', 'author'])
-        apcpaAdjMatrix = numpy.dot(apcAdjMatrix, cpaAdjMatrix)
-
-        # Correct the toNodes content in extraData
-        extraData['toNodes'] = data['toNodes']
-        extraData['toNodesIndex'] = data['toNodesIndex']
+    def runFor(self, author, adjMatrix, extraData):
 
         # Find the top 10 most similar nodes to some given node
-        mostSimilar = findMostSimilarNodes(apcpaAdjMatrix, author, extraData)
+        mostSimilar = findMostSimilarNodes(adjMatrix, author, extraData)
         self.output('\nMost Similar to "%s":' % author)
         mostSimilarTable = texttable.Texttable()
         rows = [['Author', 'Score']]
@@ -33,11 +21,23 @@ class EfficientAPCPAPathSimExperiment(Experiment):
         mostSimilarTable.add_rows(rows)
         self.output(mostSimilarTable.draw())
 
-
 if __name__ == '__main__':
     experiment = EfficientAPCPAPathSimExperiment(
         None, 'Most Similar APCPA PathSim Authors', outputFilePath='experiment/real/four_area/barebones/results/apcpaPathSim')
-    experiment.runFor('Christos Faloutsos')
-    experiment.runFor('Jiawei Han')
-    experiment.runFor('Sergey Brin')
-    experiment.runFor('Sanjay Ghemawat')
+
+    # Parse 4-area dataset graph
+    graph, nodeIndex = parseFourAreaDataset()
+
+    # Compute APCPA adjacency matrix
+    apcAdjMatrix, extraData = getMetaPathAdjacencyData(graph, nodeIndex, ['author', 'paper', 'conference'])
+    cpaAdjMatrix, data = getMetaPathAdjacencyData(graph, nodeIndex, ['conference', 'paper', 'author'])
+    apcpaAdjMatrix = numpy.dot(apcAdjMatrix, cpaAdjMatrix)
+
+    # Correct the toNodes content in extraData
+    extraData['toNodes'] = data['toNodes']
+    extraData['toNodesIndex'] = data['toNodesIndex']
+
+    experiment.runFor('Christos Faloutsos', apcpaAdjMatrix, extraData)
+    experiment.runFor('Jiawei Han', apcpaAdjMatrix, extraData)
+    experiment.runFor('Sergey Brin', apcpaAdjMatrix, extraData)
+    experiment.runFor('Sanjay Ghemawat', apcpaAdjMatrix, extraData)
