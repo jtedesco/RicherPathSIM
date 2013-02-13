3c42806b0d3a19db6e122e43e285ceb633b63e8c
Jon Tedesco <jon@jontedesco.net>
2013-02-13 01:49:30 -0600

    Added APPA PathSim example

---
+++ experiment/real/four_area/barebones/EfficientAPPAPathSimExperiment.py
@@ -0,0 +1,38 @@
+import cPickle
+import os
+import texttable
+from experiment.Experiment import Experiment
+from experiment.real.four_area.barebones.BareBonesHelper import  getMetaPathAdjacencyData, findMostSimilarNodes, getPathSimScore
+
+__author__ = 'jontedesco'
+
+class EfficientAPPANeighborSimExperiment(Experiment):
+    """
+      Runs some experiments with NeighborSim on author similarity for the 'four area' dataset
+    """
+
+    def runFor(self, author, adjMatrix, extraData):
+
+        # Find the top 10 most similar nodes to some given node
+        mostSimilar = findMostSimilarNodes(adjMatrix, author, extraData, method = getPathSimScore)
+        self.output('\nMost Similar to "%s":' % author)
+        mostSimilarTable = texttable.Texttable()
+        rows = [['Author', 'Score']]
+        rows += [[name, score] for name, score in mostSimilar]
+        mostSimilarTable.add_rows(rows)
+        self.output(mostSimilarTable.draw())
+
+
+if __name__ == '__main__':
+    experiment = EfficientAPPANeighborSimExperiment(
+        None, 'Most Similar APPA PathSim Authors', outputFilePath='experiment/real/four_area/barebones/results/appaPathSim')
+
+    # Compute once, since these never change
+    graph, nodeIndex = cPickle.load(open(os.path.join('data', 'four_area', 'graphWithCitations')))
+    appaAdjMatrix, extraData = getMetaPathAdjacencyData(graph, nodeIndex, ['author', 'paper', 'paper', 'author'])
+
+    # Run for all authors
+    experiment.runFor('Christos Faloutsos', appaAdjMatrix, extraData)
+    experiment.runFor('Jiawei Han', appaAdjMatrix, extraData)
+    experiment.runFor('Sergey Brin', appaAdjMatrix, extraData)
+    experiment.runFor('Sanjay Ghemawat', appaAdjMatrix, extraData)
