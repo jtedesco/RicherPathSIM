14b5d1a4616e1b58ac58657aa48ad7534d536645
Jon Tedesco <jon@jontedesco.net>
2013-02-15 16:23:05 -0600

    Added experiment outputs between experiment

--- experiment/real/four_area/barebones/RunAuthorsExperiments.py
+++ experiment/real/four_area/barebones/RunAuthorsExperiments.py
@@ -2,8 +2,13 @@ from experiment.real.four_area.barebones import AuthorsNeighborSimAPPAExperiment
 
 __author__ = 'jontedesco'
 
+print("Running NeighborSim APPA experiment:")
 AuthorsNeighborSimAPPAExperiment.run()
+print("Running NeighborSim CPPA experiment:")
 AuthorsNeighborSimCPPAExperiment.run()
+print("Running PathSim APCPA experiment:")
 AuthorsPathSimAPCPAExperiment.run()
+print("Running PathSim APPA experiment:")
 AuthorsPathSimAPPAExperiment.run()
+print("Running PathSim APTPA experiment:")
 AuthorsPathSimAPTPAExperiment.run()
--- experiment/real/four_area/barebones/RunPapersExperiments.py
+++ experiment/real/four_area/barebones/RunPapersExperiments.py
@@ -2,8 +2,13 @@ from experiment.real.four_area.barebones import PapersNeighborSimCPPExperiment, 
 
 __author__ = 'jontedesco'
 
+print("Running NeighborSim APP Experiment:")
 PapersNeighborSimAPPExperiment.run()
+print("Running NeighborSim CPP Experiment:")
 PapersNeighborSimCPPExperiment.run()
+print("Running PathSim PAP Experiment:")
 PapersPathSimPAPExperiment.run()
+print("Running PathSim PCP Experiment:")
 PapersPathSimPCPExperiment.run()
+print("Running PathSim PTP Experiment:")
 PapersPathSimPTPExperiment.run()
