3c7b331afa9625dbf486f5254cbfd7321f09099c
Jon Tedesco <jon@jontedesco.net>
2013-02-15 15:16:28 -0600

    Added NeighborSim CPPA experiment

---
+++ experiment/real/four_area/barebones/AuthorsNeighborSimCPPAExperiment.py
@@ -0,0 +1,57 @@
+import cPickle
+import os
+import operator
+import texttable
+from experiment.Experiment import Experiment
+from experiment.real.four_area.barebones.Helper import getMetaPathAdjacencyData, findMostSimilarNodes, getNeighborSimScore
+
+__author__ = 'jontedesco'
+
+class AuthorsNeighborSimCPPAExperiment(Experiment):
+    """
+      Runs some experiments with NeighborSim on author similarity for the 'four area' dataset
+    """
+
+    def runFor(self, paper, adjMatrix, extraData):
+
+        # Find the top 10 most similar nodes to some given node
+        mostSimilar, similarityScores = findMostSimilarNodes(adjMatrix, paper, extraData, method = getNeighborSimScore)
+        self.output('Most Similar to "%s":' % paper)
+        mostSimilarTable = texttable.Texttable()
+        rows = [['Paper', 'Score']]
+        rows += [[name, score] for name, score in mostSimilar]
+        mostSimilarTable.add_rows(rows)
+        self.output(mostSimilarTable.draw())
+
+
+if __name__ == '__main__':
+    experiment = AuthorsNeighborSimCPPAExperiment(
+        None, 'Most Similar CPPA NeighborSim Authors', outputFilePath='results/cppaNeighborSim')
+
+    # Compute once, since these never change
+    graph, nodeIndex = cPickle.load(open(os.path.join('data', 'graphWithCitations')))
+    appaAdjMatrix, extraData = getMetaPathAdjacencyData(graph, nodeIndex, ['conference', 'paper', 'paper', 'author'])
+    extraData['fromNodes'] = extraData['toNodes']
+    extraData['fromNodesIndex'] = extraData['toNodesIndex']
+
+    # Compute author citation counts
+    citationCounts = {}
+    for author in extraData['toNodes']:
+        i = extraData['toNodesIndex'][author]
+        citationCounts[author] = sum(appaAdjMatrix.getcol(i).data)
+    citationCountsList = sorted(citationCounts.iteritems(), key=operator.itemgetter(1))
+    citationCountsList.reverse()
+
+    # Output author citation counts
+    with open(os.path.join('data', 'authorCitationCounts'), 'w') as file:
+        map(lambda (author, count): file.write('%d: %s\n' % (int(count), author)), citationCountsList)
+
+    # Run for all authors (counts as of 2/15/2013)
+
+    # Productive researchers
+    experiment.runFor('Christos Faloutsos', appaAdjMatrix, extraData) # 8,279 citations, 311 papers
+    experiment.runFor('Jiawei Han', appaAdjMatrix, extraData) # 12,410 citations, 420 papers
+
+    # Industry researchers
+    experiment.runFor('Sergey Brin', appaAdjMatrix, extraData) # 3,621 citations, 13 papers
+    experiment.runFor('Sanjay Ghemawat', appaAdjMatrix, extraData) # 2,950 citations, 18 papers
