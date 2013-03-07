e59e32d63253188e245ee0e464b4f5a4e0b37a5f
Jon Tedesco <jon@jontedesco.net>
2013-03-06 20:56:13 -0600

    Fixed intermediate authors results paths

--- experiment/real/four_area/barebones/RunAuthorsExperiments.py
+++ experiment/real/four_area/barebones/RunAuthorsExperiments.py
@@ -20,8 +20,8 @@ AggregateAuthorsExperiment.run(
     os.path.join('results','authors','ppa-apcpaNeighborSim-%1.1f-%1.1f' % (0.5, 0.5)),
     citationCounts,
     publicationCounts,
-    [(os.path.join('results', 'intermediate', '%s-neighborsim-ppa'), 0.5),
-     (os.path.join('results', 'intermediate', '%s-pathsim-apcpa'), 0.5)]
+    [(os.path.join('results', 'authors', 'intermediate', '%s-neighborsim-ppa'), 0.5),
+     (os.path.join('results', 'authors', 'intermediate', '%s-pathsim-apcpa'), 0.5)]
 )
 print("Running NeighborSim PPA - PathSim APCPA experiment (0.3, 0.7):")
 AggregateAuthorsExperiment.run(
@@ -29,8 +29,8 @@ AggregateAuthorsExperiment.run(
     os.path.join('results','authors','ppa-apcpaNeighborSim-%1.1f-%1.1f' % (0.3, 0.7)),
     citationCounts,
     publicationCounts,
-    [(os.path.join('results', 'intermediate', '%s-neighborsim-ppa'), 0.3),
-     (os.path.join('results', 'intermediate', '%s-pathsim-apcpa'), 0.7)]
+    [(os.path.join('results', 'authors', 'intermediate', '%s-neighborsim-ppa'), 0.3),
+     (os.path.join('results', 'authors', 'intermediate', '%s-pathsim-apcpa'), 0.7)]
 )
 
 print("Running NeighborSim TPPA - PathSim APCPA experiment:")
@@ -39,8 +39,8 @@ AggregateAuthorsExperiment.run(
     os.path.join('results','authors','tppa-apcpaNeighborSim-%1.1f-%1.1f' % (0.5, 0.5)),
     citationCounts,
     publicationCounts,
-    [(os.path.join('results', 'intermediate', '%s-neighborsim-tppa'), 0.5),
-     (os.path.join('results', 'intermediate', '%s-pathsim-apcpa'), 0.5)]
+    [(os.path.join('results', 'authors', 'intermediate', '%s-neighborsim-tppa'), 0.5),
+     (os.path.join('results', 'authors', 'intermediate', '%s-pathsim-apcpa'), 0.5)]
 )
 print("Running NeighborSim TPPA - PathSim APCPA experiment (0.3, 0.7):")
 AggregateAuthorsExperiment.run(
@@ -48,8 +48,8 @@ AggregateAuthorsExperiment.run(
     os.path.join('results','authors','tppa-apcpaNeighborSim-%1.1f-%1.1f' % (0.3, 0.7)),
     citationCounts,
     publicationCounts,
-    [(os.path.join('results', 'intermediate', '%s-neighborsim-tppa'), 0.3),
-     (os.path.join('results', 'intermediate', '%s-pathsim-apcpa'), 0.7)]
+    [(os.path.join('results', 'authors', 'intermediate', '%s-neighborsim-tppa'), 0.3),
+     (os.path.join('results', 'authors', 'intermediate', '%s-pathsim-apcpa'), 0.7)]
 )
 
 
@@ -59,8 +59,8 @@ AggregateAuthorsExperiment.run(
     os.path.join('results','authors','absppa-apcpaNeighborSim-%1.1f-%1.1f' % (0.5, 0.5)),
     citationCounts,
     publicationCounts,
-    [(os.path.join('results', 'intermediate', '%s-neighborsim-absppa'), 0.5),
-     (os.path.join('results', 'intermediate', '%s-pathsim-apcpa'), 0.5)]
+    [(os.path.join('results', 'authors', 'intermediate', '%s-neighborsim-absppa'), 0.5),
+     (os.path.join('results', 'authors', 'intermediate', '%s-pathsim-apcpa'), 0.5)]
 )
 print("Running NeighborSim Absolute PPA - PathSim APCPA experiment (0.3, 0.7):")
 AggregateAuthorsExperiment.run(
@@ -68,6 +68,6 @@ AggregateAuthorsExperiment.run(
     os.path.join('results','authors','absppa-apcpaNeighborSim-%1.1f-%1.1f' % (0.3, 0.7)),
     citationCounts,
     publicationCounts,
-    [(os.path.join('results', 'intermediate', '%s-neighborsim-absppa'), 0.3),
-     (os.path.join('results', 'intermediate', '%s-pathsim-apcpa'), 0.7)]
+    [(os.path.join('results', 'authors', 'intermediate', '%s-neighborsim-absppa'), 0.3),
+     (os.path.join('results', 'authors', 'intermediate', '%s-pathsim-apcpa'), 0.7)]
 )
