d28df0dd0f0be8412edfddfca004c3ac196b69fb
Jon Tedesco <jon@jontedesco.net>
2013-04-10 23:32:46 -0500

    Updated authors latex conversion to highlight first entry and add footnight

--- experiment/real/four_area/barebones/results/authors/convertAuthorTablesToLatex.py
+++ experiment/real/four_area/barebones/results/authors/convertAuthorTablesToLatex.py
@@ -2,16 +2,25 @@ import os
 
 __author__ = 'jontedesco'
 
+firstPubsTemplate = '\\textbf{%d} & \\textbf{%s} & \\textbf{%s} & \\textbf{%s} & \\textbf{%s} \\\\\n'
+firstNoPubsTemplate = '\\textbf{%d} & \\textbf{%s} & \\textbf{%s} & \\textbf{%s} \\\\\n'
+middlePubsTemplate = '%d & %s & %s & %s & %s \\\\\n'
+middleNoPubsTemplate = '%d & %s & %s &  %s \\\\\n'
+
+showPublications = False
+
 for filename in os.listdir('.'):
     if 'PathSim' not in filename and 'NeighborSim' not in filename: continue
     measure = 'PathSim' if 'PathSim' in filename else 'NeighborSim'
     content = open(filename).read()
     latexContent = ""
+    rank = 0
     for line in content.split('\n'):
         if line.startswith('Most Similar'):
             rank = 0
             latexContent += '\hline\n\end{tabular}\n\n'
-            latexContent += '\\begin{tabular}{|c|c|c|c|c|}\n\hline\nRank & Author & Citations & Publications & %s Score\\\\\n\hline\n' % measure
+            latexContent += '\\begin{tabular}{|c|c|c|c|c|}\n\hline\nRank & Author & Citations \\footnotemark[1] ' \
+                            + ('& Publications ' if showPublications else '') +  '& %s Score\\\\\n\hline\n' % measure
         elif line.startswith('+='):
             continue
         elif 'Author' in line:
@@ -23,6 +32,15 @@ for filename in os.listdir('.'):
             score = tokens[1].strip()
             citations = tokens[2].strip()
             publications = tokens[3].strip()
-            latexContent += '%d & %s & %s & %s & %s \\\\\n' % (rank, author, citations, publications, score)
+            if rank == 1:
+                if showPublications:
+                    latexContent += firstPubsTemplate % (rank, author, citations, publications, score)
+                else:
+                    latexContent += firstNoPubsTemplate % (rank, author, citations, score)
+            else:
+                if showPublications:
+                    latexContent += middlePubsTemplate % (rank, author, citations, publications, score)
+                else:
+                    latexContent += middleNoPubsTemplate % (rank, author, citations, score)
     with open(os.path.join('latex', filename), 'w') as outputFile:
         outputFile.write(latexContent)
