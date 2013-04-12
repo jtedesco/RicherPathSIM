b33b6023e9d68d9d66c01096c7eb4ccd493771d9
Jon Tedesco <jon@jontedesco.net>
2013-04-12 15:26:24 -0500

    Fixed typo in variable

--- experiment/real/four_area/barebones/efficiency_experiments/TestMetaPathLengthExperiment.py
+++ experiment/real/four_area/barebones/efficiency_experiments/TestMetaPathLengthExperiment.py
@@ -67,7 +67,7 @@ def run():
             adjMatrices = []
             adjMatrix1, extraData = getMetaPathAdjacencyData(graph, nodeIndex, metaPathPart, rows=True)
             adjMatrices.append(adjMatrix1)
-            for i in repitions:
+            for i in repititions:
                 adjMatrices.append(adjMatrix1)
             partialPathsEndTime = datetime.now()
             partialTime = partialPathsEndTime - partialPathsStartTime
@@ -78,7 +78,7 @@ def run():
             # Multiply for full adj matrix
             multiplyStartTime = datetime.now()
             fullAdjMatrix = adjMatrix
-            for i in repitions:
+            for i in repititions:
                 fullAdjMatrix = fullAdjMatrix * fullAdjMatrix
             fullAdjMatrix = lil_matrix(fullAdjMatrix)
             multiplyEndTime = datetime.now()
