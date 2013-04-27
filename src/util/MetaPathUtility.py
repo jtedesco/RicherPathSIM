16b52e40dd9b36c661983bb8b063fe7ad8ff5134
Jon Tedesco <jon@jontedesco.net>
2013-04-27 13:12:16 -0500

    Cleaned up MetaPathUtility to comply with pep best practices

--- src/util/MetaPathUtility.py
+++ src/util/MetaPathUtility.py
@@ -3,13 +3,14 @@ import numpy
 
 __author__ = 'jontedesco'
 
+
 class MetaPathUtility(object):
     """
       Contains helper methods for dealing with meta paths. Note that this utility only handles meta paths that start and
       end at different nodes, and that never repeat nodes in the path (repeating meta path types is fine).
     """
 
-    def findMetaPathNeighbors(self, graph, node, metaPath, symmetric = False, checkLoops = True):
+    def findMetaPathNeighbors(self, graph, node, metaPath, symmetric=False, checkLoops=True):
         """
           Finds the neighbors of some node along the given meta path, i.e. nodes that are reachable from the given node
           along the given meta path.
@@ -23,19 +24,17 @@ class MetaPathUtility(object):
         assert(node.__class__ == metaPath[0])
 
         # Get the paths & neighbors, then add this node if necessary
-        (metaPathNeighbors, paths) = self._findMetaPathsHelper(graph, node, metaPath[1:], symmetric = symmetric)
+        (metaPathNeighbors, paths) = self._findMetaPathsHelper(graph, node, metaPath[1:], symmetric=symmetric)
         if checkLoops:
             selfLoops = self.__findLoopMetaPaths(graph, node, metaPath, symmetric)
             if len(selfLoops) > 0:
                 metaPathNeighbors.add(node)
         return set(metaPathNeighbors)
 
-
     def _findMetaPathsHelper(self, graph, node, metaPath, symmetric):
         raise NotImplementedError("Implement a concrete subclass to find meta paths!")
 
-
-    def findMetaPaths(self, graph, startingNode, endingNode, metaPath, symmetric = False):
+    def findMetaPaths(self, graph, startingNode, endingNode, metaPath, symmetric=False):
         """
           Finds all paths that match the metaPath connecting the given nodes
         """
@@ -50,8 +49,7 @@ class MetaPathUtility(object):
         else:
             return self.__findNonLoopMetaPaths(graph, startingNode, endingNode, metaPath, symmetric)
 
-
-    def expandPartialMetaPath(self, partialMetaPath, repeatLastType = False):
+    def expandPartialMetaPath(self, partialMetaPath, repeatLastType=False):
         """
           Expands a partial meta path into a full one (i.e. ABC into ABCBA or ABCCBA)
 
@@ -62,33 +60,30 @@ class MetaPathUtility(object):
 
         reversedMetaPath = list(partialMetaPath)
         reversedMetaPath.reverse()
-        metaPath = partialMetaPath if repeatLastType else partialMetaPath[:-1] # Don't repeat the middle entry
+        metaPath = partialMetaPath if repeatLastType else partialMetaPath[:-1]  # Don't repeat the middle entry
         modifiedMetaPath = metaPath + reversedMetaPath
 
         return modifiedMetaPath
 
-
-    def createHomogeneousProjection(self, graph, metaPath, symmetric = False):
+    def createHomogeneousProjection(self, graph, metaPath, symmetric=False):
         """
           Create a homogeneous graph projection of some meta path
         """
 
         # Check that the endpoints are of the correct types
         assert(metaPath[0] == metaPath[-1])
-        return self.__projectionHelper(graph, metaPath, symmetric = symmetric)
+        return self.__projectionHelper(graph, metaPath, symmetric=symmetric)
 
-
-    def createHeterogeneousProjection(self, graph, metaPath, symmetric = False):
+    def createHeterogeneousProjection(self, graph, metaPath, symmetric=False):
         """
           Create a homogeneous graph projection of some meta path
         """
 
         # Check that the endpoints are of the correct types
         assert(metaPath[0] != metaPath[-1])
-        return self.__projectionHelper(graph, metaPath, symmetric = symmetric, heterogeneous = True)
+        return self.__projectionHelper(graph, metaPath, symmetric=symmetric, heterogeneous=True)
 
-
-    def getAdjacencyMatrixFromGraph(self, graph, metaPath, project = False, symmetric = False):
+    def getAdjacencyMatrixFromGraph(self, graph, metaPath, project=False, symmetric=False):
         """
           Computes a graph projection (if meta path is provided), and the adjacency matrix
         """
@@ -118,8 +113,7 @@ class MetaPathUtility(object):
 
         return adjacencyMatrix, nodesIndex
 
-
-    def __projectionHelper(self, graph, metaPath, symmetric = False, heterogeneous = False):
+    def __projectionHelper(self, graph, metaPath, symmetric=False, heterogeneous=False):
         """
           Helper method to assist in creating a projection over a graph using some meta path
         """
@@ -133,17 +127,19 @@ class MetaPathUtility(object):
 
         # Add the edges for this graph
         for node in newGraph.getNodes():
-            if not isinstance(node, metaPath[0]): continue
+            if not isinstance(node, metaPath[0]):
+                continue
             neighbors = self.findMetaPathNeighbors(graph, node, metaPath)
             for neighbor in neighbors:
-                if not isinstance(neighbor, metaPath[-1]): continue
+                if not isinstance(neighbor, metaPath[-1]):
+                    continue
                 numPaths = len(self.findMetaPaths(graph, node, neighbor, metaPath))
                 for i in xrange(0, numPaths):
                     newGraph.addEdge(node, neighbor)
-                    if symmetric: newGraph.addEdge(neighbor, node)
+                    if symmetric:
+                        newGraph.addEdge(neighbor, node)
 
         return newGraph
-
 
     def __findLoopMetaPaths(self, graph, startingNode, metaPath, symmetric):
         """
@@ -151,7 +147,7 @@ class MetaPathUtility(object):
         """
 
         # Find reachable nodes on this shorter meta path
-        reachableNodes, paths = self._findMetaPathsHelper(graph, startingNode, metaPath[1:-1], symmetric = symmetric)
+        reachableNodes, paths = self._findMetaPathsHelper(graph, startingNode, metaPath[1:-1], symmetric=symmetric)
 
         returnPaths = []
         pathsFound = set()
@@ -179,11 +175,10 @@ class MetaPathUtility(object):
 
         return returnPaths
 
-
     def __findNonLoopMetaPaths(self, graph, startingNode, endingNode, metaPath, symmetric):
         """
           Helper function to find meta paths, given that we know the start and end nodes are not the same
         """
 
-        (metaPathNeighbors, paths) = self._findMetaPathsHelper(graph, startingNode, metaPath[1:], symmetric = symmetric)
+        (metaPathNeighbors, paths) = self._findMetaPathsHelper(graph, startingNode, metaPath[1:], symmetric=symmetric)
         return [list(path) for path in paths if path[-1] is endingNode]
