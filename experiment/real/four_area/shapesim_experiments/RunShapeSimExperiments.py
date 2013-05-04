f5f44e9afb870393520deff9a2871d7d42e36fca
Jon Tedesco <jon@jontedesco.net>
2013-05-04 16:03:26 -0500

    Added partial relative ShapeSim authors experiment

--- experiment/real/four_area/shapesim_experiments/RunShapeSimExperiments.py
+++ experiment/real/four_area/shapesim_experiments/RunShapeSimExperiments.py
@@ -1,14 +1,16 @@
 from experiment.real.four_area.shapesim_experiments import AuthorsShapeSimCPPAExperiment, \
     AuthorsShapeSimCPPAOmitCPExperiment, ConferencesShapeSimTPPCExperiment, ConferencesShapeSimTPPCExperimentRelative, \
-    AuthorsShapeSimCPPARelativeExperiment
+    AuthorsShapeSimCPPARelativeExperiment, AuthorsShapeSimCPPARelativePartialExperiment
 
 __author__ = 'jontedesco'
 
 # Standalone experiments
 print("Running ShapeSim CPPA experiment:")
 citationCounts, publicationCounts, conferenceCitations, conferencePublications = AuthorsShapeSimCPPAExperiment.run()
-print("Running ShapeSim (Relative) CPPA experiment (omit CPC):")
+print("Running ShapeSim (Relative) CPPA experiment:")
 AuthorsShapeSimCPPARelativeExperiment.run(citationCounts, publicationCounts)
+print("Running ShapeSim (Relative) CPPA experiment:")
+AuthorsShapeSimCPPARelativePartialExperiment.run(citationCounts, publicationCounts)
 print("Running ShapeSim CPPA experiment (omit CPC):")
 AuthorsShapeSimCPPAOmitCPExperiment.run(citationCounts, publicationCounts)
 print("Running ShapeSim TPPC experiment:")
