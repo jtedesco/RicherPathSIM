84873ae4c7cd4eddcdc27a3ea985d7ca8e93a14d
Jon Tedesco <jon@jontedesco.net>
2013-06-20 14:51:45 -0500

    Added more carefully weighted pathsim / shapesim experiments

--- experiment/demonstrative/skewed_citations_publications/SkewedCitationPublicationWeightedExampleExperiment.py
+++ experiment/demonstrative/skewed_citations_publications/SkewedCitationPublicationWeightedExampleExperiment.py
@@ -3,6 +3,7 @@ from experiment.Experiment import Experiment
 from src.model.node.dblp.Author import Author
 from src.model.node.dblp.Conference import Conference
 from src.model.node.dblp.Paper import Paper
+from src.similarity.AggregateSimilarityStrategy import AggregateSimilarityStrategy
 from src.similarity.heterogeneous.NeighborSimStrategy import NeighborSimStrategy
 from src.similarity.heterogeneous.RecursivePathSimStrategy import RecursivePathSimStrategy
 from src.similarity.heterogeneous.path_shape_count.FlattenedMatrixStrategy import FlattenedMatrixStrategy
@@ -33,10 +34,10 @@ class SkewedCitationPublicationWeightedExampleExperiment(Experiment):
         citationsPublications = {
             'Alice': (100, 10),
             'Bob': (80, 10),
-            'Carol': (100, 500),
-            'Dave': (1, 10),
-            'Ed': (10, 10),
-            'Frank': (1000, 100)
+            'Carol': (100, 100),
+            'Dave': (10, 10),
+            'Ed': (20, 5)
+
         }
 
         self.graph, authorMap, conference, citationsPublications = \
@@ -50,31 +51,8 @@ class SkewedCitationPublicationWeightedExampleExperiment(Experiment):
             authorMap['Bob'],
             authorMap['Carol'],
             authorMap['Dave'],
-            authorMap['Ed'],
-            authorMap['Frank']
+            authorMap['Ed']
         ]
-        metaPathUtility = EdgeBasedMetaPathUtility()
-
-        # Output adjacency matrices
-        self.output('\nCPA Adjacency Matrix:')
-        cpaadjMatrix, nodesIndex = metaPathUtility.getAdjacencyMatrixFromGraph(
-            self.graph, [Conference, Paper, Author], project=True
-        )
-        adjMatrixTable = texttable.Texttable()
-        rows = [['Conference'] + [author.name for author in authors]]
-        rows += [[conference.name] + [cpaadjMatrix[nodesIndex[conference]][nodesIndex[author]] for author in authors]]
-        adjMatrixTable.add_rows(rows)
-        self.output(adjMatrixTable.draw())
-
-        self.output('\nCPPA Adjacency Matrix:')
-        cpaadjMatrix, nodesIndex = metaPathUtility.getAdjacencyMatrixFromGraph(
-            self.graph, [Conference, Paper, Paper, Author], project=True
-        )
-        adjMatrixTable = texttable.Texttable()
-        rows = [['Conference'] + [author.name for author in authors]]
-        rows += [[conference.name] + [cpaadjMatrix[nodesIndex[conference]][nodesIndex[author]] for author in authors]]
-        adjMatrixTable.add_rows(rows)
-        self.output(adjMatrixTable.draw())
 
         # Total citation & publication counts
         self.output('\nCitation & Publication Counts')
@@ -85,11 +63,20 @@ class SkewedCitationPublicationWeightedExampleExperiment(Experiment):
         adjMatrixTable.add_rows(rows)
         self.output(adjMatrixTable.draw())
 
-        # Output ShapeSim & PathSim similarity scores
-        neighborSimStrategy = NeighborSimStrategy(self.graph, [Conference, Paper, Author], symmetric=True)
-        self.outputSimilarityScores(authorMap, authors, neighborSimStrategy, 'APCPA PathSim')
-        neighborSimStrategy = NeighborSimStrategy(self.graph, [Conference, Paper, Paper, Author])
-        self.outputSimilarityScores(authorMap, authors, neighborSimStrategy, 'APPCPPA PathSim')
+        # Output PathSim similarity scores
+        pathSimStrategyPubs = NeighborSimStrategy(self.graph, [Conference, Paper, Author], symmetric=True)
+        self.outputSimilarityScores(authorMap, authors, pathSimStrategyPubs, 'APCPA PathSim')
+        pathSimStrategyCits = NeighborSimStrategy(self.graph, [Conference, Paper, Paper, Author])
+        self.outputSimilarityScores(authorMap, authors, pathSimStrategyCits, 'APPCPPA PathSim')
+        for w1, w2 in [(0.5, 0.5), (0.6, 0.4), (0.4, 0.6)]:
+            combinedPathSimStrategy = AggregateSimilarityStrategy(
+                self.graph, [pathSimStrategyPubs, pathSimStrategyCits], [w1, w2]
+            )
+            self.outputSimilarityScores(
+                authorMap, authors, combinedPathSimStrategy, 'APCPA-APPCPPAA Pathsim (%1.1f,%1.1f)' % (w1, w2)
+            )
+
+        # Output ShapeSim strategy
         w = 1.0
         neighborPathShapeStrategy = VectorProductStrategy(
             self.graph, weight=w, omit=[], metaPath=[Conference, Paper, Paper, Author], symmetric=True
